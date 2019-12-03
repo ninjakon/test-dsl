6b1d4012f6b6d190bee576a9a87efd89045dbdfb
ninjakon <arno.breitfuss@gmx.at>
2019-12-03 21:20:05 +0100

    refactor main

--- interpreter/TestPy.py
+++ interpreter/TestPy.py
@@ -1,7 +1,4 @@
-import os
 import time
-
-from dsl.metamodel import get_metamodel
 
 
 class TText:
@@ -268,19 +265,3 @@ class TestSuite:
                color + ' == ' + TText.BOLD + '{} ' + TText.ENDC + \
                color + infix + TText.BOLD + ' {}' + TText.ENDC + \
                color + suffix + TText.ENDC
-
-
-if __name__ == '__main__':
-    meta = get_metamodel()
-    model = meta.model_from_file(os.getcwd() + '/test/models/model_big.test')
-
-    test_suite = TestSuite(verbose=False)
-    test_suite.interpret(model)
-    print(test_suite)
-
-    # single test
-    test_suite.run_test('databaseQueries')
-    print(test_suite)
-    # all tests
-    test_suite.run_all()
-    print(test_suite)
