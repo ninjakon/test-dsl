a82159d07148dbd1a1714f222603adfe9c72d7d5
ninjakon <arno.breitfuss@gmx.at>
2019-12-05 21:49:23 +0100

    init actors with attrs

--- interpreter/TestHelper.js
+++ interpreter/TestHelper.js
@@ -3,25 +3,61 @@ const argType = {
     tests: 3
 };
 
+const testBlock = {
+  before: 0,
+  execute: 1,
+  after: 2,
+};
+
+/**
+ * Get arguments for testing.
+ */
 var actor_definitions = JSON.parse(process.argv[argType.actor_definitions]);
 var tests = JSON.parse(process.argv[argType.tests]);
 
-console.log(actor_definitions);
-console.log(tests);
-
-// load actors
+/**
+ * Load actors by specifying modules, instantiating classes and setting attributes.
+ */
 var actors = {};
 for (var actor_name in actor_definitions) {
+    // instantiate classes
     var module = actor_definitions[actor_name][0];
     const actor_class = require(module);
     actors[actor_name] = new actor_class();
+
+    // set pre-defined attributes
+    var attributes = actor_definitions[actor_name][1];
+    for (var i = 0; i < attributes.length; i++) {
+        var attribute_name = attributes[i][0];
+        var value = attributes[i][1];
+        actors[actor_name][attribute_name] = value;
+    }
 }
 
-console.log(actors);
+/**
+ * Run tests.
+ */
+for (var test_name in tests) {
+    console.log('Running ' + test_name);
+    var steps = tests[test_name][testBlock.execute];
+    for (var i = 0; i < steps.length; i++) {
+        step = steps[i];
+        type = step[0];
 
-// run tests
+        switch (type) {
+            case 'AssertStep':
+                console.log(step);
+                break;
+            case 'AssignStep':
+                console.log("ASSIGN");
+                break;
+            case 'CallStep':
+                console.log("CALL");
+                break;
+            case 'TimeStep':
+                console.log("TIME");
+                break;
 
-
-function run_test(test) {
-    console.log(test);
+        }
+    }
 }
