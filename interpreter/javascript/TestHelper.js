841332bd413a782c206caededd8cc5de972db93e
ninjakon <arno.breitfuss@gmx.at>
2019-12-06 15:55:20 +0100

    clean up js

--- interpreter/javascript/TestHelper.js
+++ interpreter/javascript/TestHelper.js
@@ -25,14 +25,14 @@ const tText = {
 };
 
 /* *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  Log */
-var log = {
+const log = {
     raw_text: '',
     actor_table: null
 };
 
-function print_if_verbose(msg, tb_lvl=0) {
+function print_if_verbose(msg, tb_lvl = 0) {
     if (verbose) {
-        for (var i = 0; i < tb_lvl; i++) {
+        for (let i = 0; i < tb_lvl; i++) {
             log.raw_text += '\t';
         }
         log.raw_text += msg + tText.ENDC + '\n';
@@ -40,7 +40,7 @@ function print_if_verbose(msg, tb_lvl=0) {
 }
 
 /* *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *Test Report */
-var test_report = {
+const test_report = {
     BA: [],
     TC: 0,
     CT: [null, []],
@@ -49,7 +49,7 @@ var test_report = {
     AA: []
 };
 
-add_error_fun = null;
+let add_error_fun = null;
 
 /*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  Testing Arguments */
 const actor_definitions = JSON.parse(process.argv[argType.actor_definitions]);
@@ -62,17 +62,16 @@ const verbose = process.argv[argType.verbose] === 'True';
 
 /*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *Testing */
 function process_ba_aa(type, steps, callback) {
-    var is_ba = type.localeCompare('BA') === 0;
-    print_if_verbose(tText.INFO + (is_ba ? 'Running BeforeAll' : 'Running AfterAll'));
-    add_error_fun = function(l, e) { test_report[type].push([l + tText.ENDC, e + tText.ENDC]) };
-    process_steps(steps, is_ba=true, callback);
+    print_if_verbose(tText.INFO + (type.localeCompare('BA') === 0 ? 'Running BeforeAll' : 'Running AfterAll'));
+    add_error_fun = (l, e) => { test_report[type].push([l + tText.ENDC, e + tText.ENDC]) };
+    process_steps(steps, true, callback);
 }
 
 function process_before_after_test(type, blocks, references, callback) {
     if (references.length > 0) {
-        print_if_verbose(tText.INFO + 'Running ' + type, tb_lvl=1);
+        print_if_verbose(tText.INFO + 'Running ' + type, 1);
     }
-    add_error_fun = function (l, e) {
+    add_error_fun = (l, e) => {
         test_report.CT[1].push([tText.INFO + 'In ' + type + ' Clause: ' + l + tText.ENDC, e + tText.ENDC])
     };
     const recursive_b_a = (i) => {
@@ -88,10 +87,10 @@ function process_before_after_test(type, blocks, references, callback) {
 function process_tests(test_names, callback) {
     const recursive_test = (i) => {
         if (i < test_names.length) {
-            var test_name = test_names[i];
+            let test_name = test_names[i];
             log.raw_text += tText.INFO + 'Running ' + test_name + tText.ENDC + '\n';
             test_report.CT = [test_name, []];
-            process_test(test_name, i + 1, recursive_test);
+            process_test(test_name, () => recursive_test(i + 1));
         } else {
             callback();
         }
@@ -99,37 +98,41 @@ function process_tests(test_names, callback) {
     recursive_test(0);
 }
 
-function process_test(test_name, i, callback) {
-    var test = tests[test_name];
+function process_test(test_name, callback) {
+    const test = tests[test_name];
+    /* execute before test -> callback: tests */
     process_before_after_test('Before', befores, test[testBlock.before], () => {
-        print_if_verbose(tText.INFO + 'Executing Test', tb_lvl=1);
-        add_error_fun = (l, e) => test_report.CT[1].push([l + tText.ENDC, e + tText.ENDC]);
-        process_steps(test[testBlock.execute], is_ba=false, () => {
+        /* execute test -> callback: after test */
+        process_steps(test[testBlock.execute], false, () => {
+            /* execute after test -> callback: next test */
             process_before_after_test('After', afters, test[testBlock.after], () => {
-                var current_test = test_report.CT[0];
-                var errors = test_report.CT[1];
+                const errors = test_report.CT[1];
                 if (errors.length === 0) {
-                    test_report.ST.push(current_test);
+                    test_report.ST.push(test_report.CT[0]);
                 } else {
                     test_report.FT.push(test_report.CT);
                 }
                 test_report.TC += 1;
                 test_report.CT = [null, []];
 
-                callback(i);
+                callback();
             });
         });
     });
 }
 
 /* *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  Step Processing */
-function process_steps(steps, is_ba=false, callback) {
+function process_steps(steps, is_ba, callback) {
+    if (!is_ba) {
+        print_if_verbose(tText.INFO + 'Executing Test', 1);
+        add_error_fun = (l, e) => test_report.CT[1].push([l + tText.ENDC, e + tText.ENDC]);
+    }
     const recursive_step = (i) => {
         if (i < steps.length) {
             if (is_ba) {
-                print_if_verbose(tText.INFO + '"' + steps[i][0] + '" OK', tb_lvl=2);
+                print_if_verbose(tText.INFO + '"' + steps[i][0] + '" OK', 2);
             }
-            process_step(steps[i][0], steps[i][1], i + 1, recursive_step);
+            process_step(steps[i][0], steps[i][1], () => recursive_step(i + 1));
         } else {
             callback();
         }
@@ -137,67 +140,67 @@ function process_steps(steps, is_ba=false, callback) {
     recursive_step(0);
 }
 
-function process_step(type, args, i, callback) {
+function process_step(type, args, callback) {
     switch (type) {
         case 'AssertStep':
-            process_assert_step(args, i, callback);
+            process_assert_step(args, callback);
             break;
         case 'AssignStep':
-            process_assign_step(args, i, callback);
+            process_assign_step(args, callback);
             break;
         case 'CallStep':
-            process_call_step(args, i, callback);
+            process_call_step(args, callback);
             break;
         case 'TimeStep':
-            process_time_step(args, i, callback);
+            process_time_step(args, callback);
             break;
     }
 }
 
 /* *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * Specified Step Processing */
-function process_assert_step(args, i, callback) {
-    actor_name = args[0];
-    actor = actors[actor_name];
-    attribute = args[1];
-    expected_value = args[2];
-    actual_value = actor[attribute];
+function process_assert_step(args, callback) {
+    const actor_name = args[0];
+    const actor = actors[actor_name];
+    const attribute = args[1];
+    const expected_value = args[2];
+    const actual_value = actor[attribute];
 
-    is_ok = actual_value === expected_value;
-    message = style_assertion(is_ok, actor_name, attribute, expected_value, actual_value);
+    const is_ok = actual_value === expected_value;
+    const message = style_assertion(is_ok, actor_name, attribute, expected_value, actual_value);
     if (!is_ok) {
         add_error_fun('IDK', message + tText.ENDC);
     }
-    print_if_verbose(message, tb_lvl=2);
-    callback(i);
+    print_if_verbose(message, 2);
+    callback();
 }
 
-function process_assign_step(args, i, callback) {
-    actor = actors[args[0]];
-    attribute = args[1];
-    value = args[2];
-    actor[attribute] = value;
-    callback(i);
+function process_assign_step(args, callback) {
+    const actor = actors[args[0]];
+    const attribute = args[1];
+    actor[attribute] = args[2];
+    callback();
 }
 
-function process_call_step(args, i, callback) {
-    actor = actors[args[0]];
-    method = args[1];
-    params = [];
-    for (var j = 0; j < args[2].length; j++) {
-        is_actor = args[2][j][0];
-        param = args[2][j][1];
+function process_call_step(args, callback) {
+    const actor = actors[args[0]];
+    const method = args[1];
+    const params = [];
+    for (let j = 0; j < args[2].length; j++) {
+        const is_actor = args[2][j][0];
+        const param = args[2][j][1];
         is_actor ? params.push(actors[param]) : params.push(param);
     }
     actor[method](...params);
-    callback(i);
+    callback();
 }
 
-function process_time_step(args, i, callback) {
-    setTimeout(() => callback(i), args[0]);
+function process_time_step(args, callback) {
+    setTimeout(callback, args[0]);
 }
 
 /*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *Assertions */
 function style_assertion(is_ok, actor_name, attribute, expected_value, actual_value) {
+    let prefix, infix, suffix, color;
     if (is_ok) {
         prefix = tText.OK + 'Assertion OK: ' + tText.ENDC;
         infix = 'and was';
@@ -217,20 +220,24 @@ function style_assertion(is_ok, actor_name, attribute, expected_value, actual_va
 }
 
 /*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * Actors */
-var actors = {};
-var actor_tbl = [];
-for (var actor_name in actor_definitions) {
+const actors = {};
+const actor_tbl = [];
+for (let actor_name in actor_definitions) {
+    /* check for IDE */
+    if (!actor_definitions.hasOwnProperty(actor_name)) {
+        break;
+    }
+
     /* instantiate classes */
-    var module = actor_definitions[actor_name][0];
+    const module = actor_definitions[actor_name][0];
     const actor_class = require(module);
     actors[actor_name] = new actor_class();
 
     /* set pre-defined attributes */
-    var attributes = actor_definitions[actor_name][1];
-    for (var i = 0; i < attributes.length; i++) {
-        var attribute_name = attributes[i][0];
-        var value = attributes[i][1];
-        actors[actor_name][attribute_name] = value;
+    const attributes = actor_definitions[actor_name][1];
+    for (let i = 0; i < attributes.length; i++) {
+        const attribute_name = attributes[i][0];
+        actors[actor_name][attribute_name] = attributes[i][1];
     }
 
     /* append to actor table */
